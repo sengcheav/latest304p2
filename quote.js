c915459b3e7e9c1b726cb371f1bdd2b167a6891a
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-23 19:39:21 +1200

    get from db

--- quote.js
+++ quote.js
@@ -43,12 +43,21 @@ app.get('/quote/random', function(req, res) {
   res.send(q);
 });
 
-app.get('/quote/:id', function(req, res) {
+/*app.get('/quote/:id', function(req, res) {
   if(quotes.length <= req.params.id || req.params.id < 0) {
     res.statusCode = 404;
     return res.send('Error 404: No quote found');
   }
 
+  var q = quotes[req.params.id];
+  res.send(q);
+});*/
+
+app.get('/quote/:id', function(req, res) {
+  if(quotes.length <= req.params.id || req.params.id < 0) {
+    res.statusCode = 404;
+    return res.send('Error 404: No quote found');
+  }
   var q = quotes[req.params.id];
   res.send(q);
 });
