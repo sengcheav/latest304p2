b059f01373942afdb721d68fccc8ad51eed1bccb
Seng Leat <leatseng@barretts.ecs.vuw.ac.nz>
2015-05-30 14:28:46 +1200

    m

--- quote.js
+++ quote.js
@@ -57,7 +57,7 @@ app.get('/quote/random', function(req, res) {
 
 app.get('/quote/all/', function(req,res){
 var all = []; 
-query = client.query('SELECT * FROM quote);
+query = client.query('SELECT * FROM quote');
 query.on( 'row', function(result){
 if(!result){ res.statusCode =404 ; return res.send('Error 404');}
 else {console.log(result);  all.push(result);}
